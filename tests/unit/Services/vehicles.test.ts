029dd5e03a79272e409d6a6b0b9114490467d122
Jean Paul Bernhardt <beralb@protonmail.com>
2023-01-16 12:05:25 -0300

    testes unitarios - 03

--- tests/unit/Services/vehicles.test.ts
+++ tests/unit/Services/vehicles.test.ts
@@ -1,10 +1,8 @@
 import { expect } from 'chai';
 import sinon from 'sinon';
 import { Model } from 'mongoose';
-// import CarODM from '../../../src/Models/CarODM';
 import CarService from '../../../src/Services/CarService';
 import MotorcycleService from '../../../src/Services/MotorcycleService';
-// import { mockCar, mockCarsArray, mockMotorcyclesArray } from '../../mocks/TestMocks';
 import { mockCarsArray, mockMotorcyclesArray } from '../../mocks/TestMocks';
 
 describe('Testa a busca de carros', function () {
@@ -36,33 +34,4 @@ describe('Testa a busca de carros', function () {
 
     sinon.restore();
   });
-
-  // it('Deveria buscar todas carros pelo seu ID com SUCESSO', async function () {
-  //   sinon.stub(Model, 'findById').resolves(mockCar);
-
-  //   const searchedCar = mockCar;
-  //   // console.log('🚀 ~ file: vehicles.test.ts:13 ~ searchedCar', searchedCar);
-  //   const service = new CarService();
-  //   // console.log('🚀 ~ file: vehicles.test.ts:45 ~ service', service);
-  //   const result = await service.getById('63c52df6e4f1c5b7839ed90b');
-
-  //   expect(result).to.be.equal(searchedCar);
-
-  //   sinon.restore();
-  // });
-
-  // it('Deveria buscar todas carros pelo seu ID com SUCESSO', async function () {
-  //   const carModel = new CarODM();
-  //   sinon.stub(carModel, 'findById').resolves(mockCar);
-
-  //   const searchedCar = mockCar;
-  //   // console.log('🚀 ~ file: vehicles.test.ts:13 ~ searchedCar', searchedCar);
-  //   const service = new CarService();
-  //   // console.log('🚀 ~ file: vehicles.test.ts:45 ~ service', service);
-  //   const result = await service.getById('63c52df6e4f1c5b7839ed90b');
-
-  //   expect(result).to.be.equal(searchedCar);
-
-  //   sinon.restore();
-  // });
 });
